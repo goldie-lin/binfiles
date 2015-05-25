e361a3e4af28d2273dd4cd857c0cafe2fa884b67
Goldie Lin <731064+goldie-lin@users.noreply.github.com>
2015-05-25 16:58:19 +0800

    add get_android_makefile_define_macros.sh

---
+++ get_android_makefile_define_macros.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Author: Goldie Lin
+# Usage: cd to Android's root folder and run this script.
+
+def_file="build/core/definitions.mk"
+out_file="android_makefile_define_macros.list"
+
+check_pwd() {
+  [[ ! -f "Makefile" ]] && echo >&2 "Makefile not found! (Please cd to Android root)" && return 1
+}
+
+gen_def_list() {
+  local def_list=""
+
+  def_list="$(grep '^define' "${def_file}" | cut -d' ' -f2- | sort -u)"
+  echo "${def_list}" | tee "${out_file}"
+}
+
+main() {
+  check_pwd
+  gen_def_list
+}
+
+main "$@"
